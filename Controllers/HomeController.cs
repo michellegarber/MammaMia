49aaebc9b8843ee1b60c51a575cccd56ce94f010
MICHELLE SOFIA GARBER <48521612@ort>
2024-07-29 10:30:10 -0300

    first commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,9 +21,52 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult Inicio()
+    {
+        return View();
+    }
+
+    public IActionResult Victoria()
+    {
+        return View();
+    }
+    public IActionResult Creditos()
+    {
+        return View();
+    }
+    
     public IActionResult Comenzar()
     {
         string habitacion = "habitacion" +  Escape.GetEstadoJuego();
         return View (habitacion);
+    }
+    public IActionResult Habitacion(int sala, string clave){
+        if(sala != Escape.GetEstadoJuego()){
+            return View("habitacion" + Escape.GetEstadoJuego());
+        }else{
+            if(Escape.ResolverSala(sala, clave)){
+                return View("habitacion" +  Escape.GetEstadoJuego());
+            }
+            else if(sala == 4){
+                return View("Victoria");
+            }
+            else{
+                ViewBag.Error="ERROR";
+                return View("habitacion" +  Escape.GetEstadoJuego());
+            }
+        }
+    }
+
+    public IActionResult Habitacion1(){
+        return View();
+    }
+    public IActionResult Habitacion2(){
+        return View();
+    }
+    public IActionResult Habitacion3(){
+        return View();
+    }
+    public IActionResult Habitacion4(){
+        return View();
     }
 }
--- Models/Escape.cs
+++ Models/Escape.cs
@@ -1,28 +1,29 @@
 class Escape{
     //Atributos
     private static string[] incognitasSalas = new string[4];
-    private static int estadoJuego;
+    private static int estadoJuego = 1;
 
     //Métodos
     private static void InicializarJuego(){
-        incognitasSalas[0] = "21108";
-        incognitasSalas[1] = "70189";
-        incognitasSalas[2] = "56279";
-        incognitasSalas[3] = "35791";
+        incognitasSalas[0] = "84747";
+        incognitasSalas[1] = "gpcai";
+        incognitasSalas[2] = "afrodita";
+        incognitasSalas[3] = "bill";
+        
     }
 
     public static int GetEstadoJuego(){
-        //si es necesario inicializarlo en 1
         return estadoJuego;
     }
     public static bool ResolverSala(int Sala, string Incognita){
         bool resuelto = false;
-        if(incognitasSalas.Length == 0){
+        if(incognitasSalas[0] == null){
             InicializarJuego();
         }
         if (Sala == estadoJuego && Incognita == incognitasSalas[Sala-1])
         {
             resuelto = true;
+            estadoJuego++;
         }
         return resuelto;
 
